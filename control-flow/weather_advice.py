aeb650b334a00d17fbd9610753ba14dc764b5989
Elshaday021 <elshaday.basie@berhanbanksc.com>
2025-05-26 06:51:39 -0700

    One print line added

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -11,3 +11,5 @@ elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
+
+print("All has been done")
